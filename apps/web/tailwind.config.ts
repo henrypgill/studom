9ac3bc9c090050b5b19fe73e9ed3b9e8ced5c475
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-11-08 12:16:21 +0000

    corrected path for tailwin config of web app

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@studom/tailwind-config";
-const config: Pick<Config, "presets"> = {
+
+const config: Config = {
   presets: [sharedConfig],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
 };
 
 export default config;
